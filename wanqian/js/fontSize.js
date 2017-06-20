c34b400548b4ecf65a7ff84099055f8dc31d4f50
yyyyyyhang <2047146853@qq.com>
2017-06-20 09:28:55 +0800

    aaa

---
+++ wanqian/js/fontSize.js
@@ -0,0 +1,9 @@
+/**
+ *   Created by Administrator on 2017/5/10.
+ */
+(function () {
+    var width = screen.width;
+    var html = document.querySelector("html");
+    html.style.fontSize = width / 15 + "px";
+    //    @fs:50rem;
+})();
